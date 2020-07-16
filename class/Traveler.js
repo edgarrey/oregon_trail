b94bdd2e86bf3332261b57a2ad42627aa346dd19
Edgar <some1just@gmail.com>
2020-07-16 09:44:33 -0600

    added Traveler

--- class/Traveler.js
+++ class/Traveler.js
@@ -1,21 +1,32 @@
 class Traveler {
-    constructor (name, food, isHealthy){
+    constructor (name){
         this.name= name
-        this.food = food
-        this.isHealthy= isHealthy
+        this.food = 1
+        this.isHealthy= true
     }
 
-    food(){
-        let (i= 1; i> 2 i++){
-            
-        }
+    hunt(){
+       this.food += 2
     }
 
 
-    eat (food){
-        if (eat){
-            return eat + 2
-        }
-    }
+eat(){
+    if (this.food === 0){
+        this.isHealthy = false
+     }else {
+         this.food -= 1
+     }
+}
         
 }
+
+let bob = new Traveler ('Bob')
+console.log(bob)
+bob.hunt()
+console.log(bob)
+bob.eat()
+console.log(bob)
+bob.eat()
+bob.eat()
+bob.eat()
+console.log(bob)
--- class/Wagon.js
+++ class/Wagon.js
@@ -2,6 +2,10 @@ class Wagon{
     constructor(capacity, passengers){
         this.capacity= capacity
         this.passengers= passengers
-    }
     
-}
+    getAvailableSeatCount(){
+        
+    }
+ }
+    
+// use should quarantine should use a for loop
